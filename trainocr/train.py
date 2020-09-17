48e8118921f4d76005266ef8517f5ed594d8ebf9
Kamui <qntmmagic7@gmail.com>
2020-09-16 20:03:09 -0500

    added error handling and logs

--- trainocr/train.py
+++ trainocr/train.py
@@ -12,14 +12,14 @@ MAX_ITERATIONS=10
 TRAIN_LANG="jpn"
 
 def get_font_name(path):
-    font_name = check_output('fc-scan --format "%{family}" "' + path + '"', shell=True).decode("utf-8")
+    font_name = check_output('fc-scan --format "%{family} %{style}" "' + path + '"', shell=True).decode("utf-8")
     return font_name.split(",")[0]
 
 def trim_font_name(name):
     return name.replace("", "_")
 
 def gen_train_data(fontname):
-    return check_call(f'tesseract/src/training/tesstrain.sh --fonts_dir fonts --tessdata_dir  {TESSDATA_DIR} --langdata_dir {LANG_DATA_DIR} --linedata_only --lang jpn --fontlist "JF Dot M+H 12" --noextract_font_properties --output_dir train --exposures "0"', shell=True)    
+    return check_call(f'tesseract/src/training/tesstrain.sh --fonts_dir fonts --tessdata_dir  {TESSDATA_DIR} --langdata_dir {LANG_DATA_DIR} --linedata_only --lang jpn --fontlist "{fontname}" --noextract_font_properties --output_dir train --exposures "0"', shell=True)    
 
 def extract_lstm(traineddata):
     return check_call(f'combine_tessdata -e "{traineddata}" jpn.lstm', shell=True) 
@@ -54,11 +54,19 @@ for font in fonts:
     slim_fname = trim_font_name(font_name)
 
     clean_trainfiles()
-    gen_train_data(font_name)
+    try:
+        gen_train_data(font_name)
 
-    extract_lstm(old_tdata)
-    finetune(old_tdata, font_name)
-    combine_traineddata(slim_fname, old_tdata, slim_fname)
-    trained.append({"fname": font_name, "slim_fname": slim_fname, "tdata": slim_fname+".traineddata"})
+        extract_lstm(old_tdata)
+        finetune(old_tdata, font_name)
+        combine_traineddata(slim_fname, old_tdata, slim_fname)
+    except:
+        with open("train.log", "a", encoding="utf-8") as f:
+            f.write(f"Bad font: ${font_name}\n")
+        continue
+    else:
+        trained.append({"fname": font_name, "slim_fname": slim_fname, "tdata": slim_fname+".traineddata"})
+        with open("train.log", "a", encoding="utf-8") as f:
+            f.write(f"Successfully trained font: ${font_name}\n")
 
 print(trained)
