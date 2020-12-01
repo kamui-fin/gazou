f36eb68603b7fd6dc58bad15cec98af3e73203db
kamui-7 <65237048+kamui-7@users.noreply.github.com>
2020-11-30 18:38:03 -0600

    added more image processing

--- core/src/ocr.cpp
+++ core/src/ocr.cpp
@@ -3,7 +3,7 @@
 OCR::OCR()
 {
     tess = new tesseract::TessBaseAPI();
-    tess->Init(NULL, "jpn_vert", tesseract::OEM_LSTM_ONLY);
+    tess->Init("/home/kamui/Coding/Projects/JP_OCR/core/data/models", "jpn_vert", tesseract::OEM_LSTM_ONLY);
     tess->SetPageSegMode(tesseract::PSM_SINGLE_BLOCK_VERT_TEXT);
     tess->SetVariable("tessedit_char_blacklist", "}><L");
     tess->SetVariable("textord_debug_tabfind", "0");
@@ -17,6 +17,7 @@ OCR::OCR()
     tess->SetVariable("tessedit_class_miss_scale", "0.002");
     tess->SetVariable("textord_initialx_ile", "1.0");
     tess->SetVariable("preserve_interword_spaces", "1");
+    tess->SetVariable("user_defined_dpi", "300");
 }
 
 OCR::~OCR()
@@ -29,15 +30,41 @@ OCR::~OCR()
 cv::Mat OCR::processImage(char const *path)
 {
 
-    cv::Mat image = cv::imread(path, cv::IMREAD_COLOR);
-    // cv::resize(image, image, cv::Size(), 1.2, 1.2, cv::INTER_CUBIC);
-    // cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
-    // cv::dilate(image, image, cv::Mat());
-    // cv::erode(image, image, cv::Mat());
-    // cv::medianBlur(image, image, 3);
-    // cv::adaptiveThreshold(image, image, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 31, 2);
-    cv::imwrite("/home/kamui/Coding/Projects/JP_OCR/core/data/images/afterProcess.jpg", image);
-    return image;
+    cv::Mat image = cv::imread(path, cv::IMREAD_GRAYSCALE);
+
+    int factor = std::max(1, 1800 / image.size().width);
+    cv::resize(image, image, cv::Size(), factor, factor, cv::INTER_CUBIC);
+
+    // remove noise and smooth
+    cv::Mat filtered;
+    cv::adaptiveThreshold(image, filtered, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 41, 3);
+
+    cv::Mat opening;
+    cv::morphologyEx(filtered, opening, cv::MORPH_OPEN, {{1}});
+
+    cv::Mat closing;
+    cv::morphologyEx(opening, closing, cv::MORPH_CLOSE, {{1}});
+
+    cv::Mat th1;
+    cv::threshold(image, th1, 180, 255, cv::THRESH_BINARY);
+
+    cv::Mat th2;
+    cv::threshold(th1, th2, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
+
+    cv::Mat blur;
+    cv::Size size = {1, 1};
+    cv::GaussianBlur(th2, blur, size, 0);
+
+    cv::threshold(blur, image, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
+
+    cv::Mat finalImage;
+    cv::bitwise_or(image, closing, finalImage);
+
+    cv::imwrite("/home/kamui/Coding/Projects/JP_OCR/core/data/images/afterProcess.jpg", finalImage);
+
+    cv::cvtColor(finalImage, finalImage, cv::COLOR_GRAY2BGR);
+
+    return finalImage;
 }
 
 void OCR::extractText(cv::Mat *img)
