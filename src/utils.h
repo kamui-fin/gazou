71bdd336957a5a4fa2bffa8782413d29d42e3a63
kamui-7 <65237048+kamui-7@users.noreply.github.com>
2020-12-26 18:27:15 -0600

    setup clean exit

--- src/utils.h
+++ src/utils.h
@@ -1,4 +1,9 @@
 #include <QScreen>
+#include <QHotkey>
+#include <QClipboard>
+#include <map>
 
 QScreen *getActiveScreen();
 void remove_spaces(char* s);
+void setRegistered(std::map<std::string, QHotkey *> hotkeys, bool registered);
+void copyToClipboard(char *text, QClipboard *cb);
